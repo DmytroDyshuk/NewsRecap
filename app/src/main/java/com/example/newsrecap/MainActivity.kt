c76f16e319bfe428d871eb59c54f94c13bba6cbf
DmitriyDyshuk <dd.kronos@gmail.com>
2023-03-08 17:00:00 +0200

    implementation components, themes colors update

---
+++ app/src/main/java/com/example/newsrecap/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.newsrecap
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.newsrecap.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
